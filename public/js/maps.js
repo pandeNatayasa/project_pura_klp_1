9a87568805b7e768bf189542d7b3b24ca6878edd
bayuguna <putubayuguna@gmail.com>
2019-05-02 09:46:24 +0800

    geolocation

--- public/js/maps.js
+++ public/js/maps.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         iconAnchor:   [30, 30], // point of the icon which will correspond to marker's location
         popupAnchor:  [-15, -30] // point from which the popup should open relative to the iconAnchor
     });		
+
     var marker;
     var markers = L.markerClusterGroup();
     var map = L.map('map',{
@@ -12,6 +13,11 @@ $(document).ready(function(){
     }).setView([-8.5240574,115.2110998],11);	
     L.tileLayer('https://maps.tilehosting.com/styles/streets/{z}/{x}/{y}.png?key=YrAn6SOXelkLFXHv03o2',{
         attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
+    }).addTo(map);
+    lc = L.control.locate({
+        strings: {
+            title: "Show me where I am, yo!"
+        }
     }).addTo(map);
 
     map.on('mousemove',function(e){
@@ -25,12 +31,8 @@ $(document).ready(function(){
     $("#zoomout").click(function(){
         map.zoomOut(1);
     });
-    $("#center").click(function(){
-        map.setView([-8.5240574,115.2110998],map.getZoom());
-    });
 
 
-    // $("#s_OSM").prop("checked",true);	
     $("#s_OSM").on("click",function(){
         L.tileLayer('https://maps.tilehosting.com/styles/streets/{z}/{x}/{y}.png?key=YrAn6SOXelkLFXHv03o2',{
             attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
@@ -64,9 +66,33 @@ $(document).ready(function(){
             subdomains:['mt0','mt1','mt2','mt3']
         }).addTo(map);
     });
+
+    map.locate({setView: true, maxZoom: 16});
+
+    function onLocationFound(e) {
+        // var radius = e.accuracy / 1;
+    
+        L.marker(e.latlng).addTo(map)
+    
+        // L.circle(e.latlng, radius).addTo(map);
+        $("#center").click(function(){
+            map.setView(e.latlng,map.getZoom());
+        });
+    }
+    
+    map.on('locationfound', onLocationFound);
+
+    function onLocationError(e) {
+        alert(e.message);
+    }
+    
+    map.on('locationerror', onLocationError);
+    
     
     var marker1 = L.marker([-8.708337,115.185124],{icon: icons}).addTo(map).bindPopup(
     "<b>Pura Goa Gong</b></br>"+
     "Alamat : Jl. Pura Mertasari I No.1, Pemecutan Klod, Kuta, Kabupaten Badung, Bali 80361</br>"+
     "Telp : 0812-1981-1988</br>").openPopup();
     });	
+
+    
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,32 +5,35 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Responsive sidebar template with sliding effect and dropdown menu based on bootstrap 3">
+    <meta name="description" content="Maps Temple">
     <title>Temple Information</title>
+    <!-- Icon -->
     <link rel="icon" href="/user/temple-icon.png">
+    <!-- Bootstrap 4 -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
         crossorigin="anonymous">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Leaflet -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js"></script>
     <link rel="stylesheet" href="https://leaflet.github.io/Leaflet.markercluster/dist/MarkerCluster.css" />
     <link rel="stylesheet" href="https://leaflet.github.io/Leaflet.markercluster/dist/MarkerCluster.Default.css" />
     <script src="https://leaflet.github.io/Leaflet.markercluster/dist/leaflet.markercluster-src.js"></script>
-    <link rel="stylesheet" href="/css/sidenav.css">
+    <!--Google Fonts-->
     <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Wendy+One" rel="stylesheet">
-    <link rel="stylesheet" href="/css/carousel.css">
- 
+
+    <!--Custome Css-->
+    <link rel="stylesheet" href="/css/sidenav.css">
 
 </head>
 
 <body>
 <div class="page-wrapper chiller-theme toggled">
+    <!--Sidebar-->
     <nav id="sidebar" class="sidebar-wrapper">
+        <!-- Main Sidebar-->
         <div class="sidebar-content">
-        {{-- <div class="sidebar-brand p-2">
-            <a class="text-center ">Temple Information</a>
-        </div> --}}
         <div class="sidebar-menu">
             <div>
                 <img src="/user/uluwatu.jpg" alt="" width="100%" height="200" style="">
@@ -48,28 +51,28 @@
             <div class="container mt-3">
                 <p class="mb-1"><img src="/user/element.png" width="21" alt="" class="mb-1"><span class="mx-1"></span> Element Pura :</p>
                 <div class="row">
-                    <div class="element col-4 mb-2 p-1">
+                    <div class="element col-4 mb-2">
                         <img src="/user/element/element1.1.jpg" width="90px" height="50px" alt="Card image">
                     </div>
-                    <div class="element2 col-4 p-1">
+                    <div class="element2 col-4">
                             <img src="/user/uluwatu.jpg" width="90px" height="50px" alt="Card image">
                     </div>
-                    <div class="col-4 p-1">
+                    <div class="col-4">
                         <a href="#">
                             <img src="/user/uluwatu.jpg" width="90px" height="50px" alt="Card image">
                         </a>
                     </div>
-                    <div class="col-4 p-1">
+                    <div class="col-4">
                         <a href="#">
                             <img src="/user/uluwatu.jpg" width="90px" height="50px" alt="Card image">
                         </a>
                     </div>
-                    <div class="col-4 p-1">
+                    <div class="col-4">
                         <a href="#">
                             <img src="/user/uluwatu.jpg" width="90px" height="50px" alt="Card image">
                         </a>
                     </div>
-                    <div class="col-4 p-1">
+                    <div class="col-4">
                         <a href="#">
                             <img src="/user/uluwatu.jpg" width="90px" height="50px" alt="Card image">
                         </a>
@@ -79,6 +82,7 @@
         </div>
         </div>
 
+        <!-- Element Sidebar-->
         <div id="myElement" class="hide">
             <div class="sidebar-brand p-1 mx-auto">
                 <button class="btn btn-default bg-white" id="close-element">
@@ -117,19 +121,20 @@
             </div>
         </div>
     </nav>
-    <!-- sidebar-wrapper  -->
+
+    <!-- Main Content -->
     <main class="page-content p-0" style="height:100%">
-        {{-- <div class="card p-0" style="background: #DFE9F6;"> --}}
-            <div class="login-floating">
-                    <ul class="row  mr-2 p-0 ">
-                        <a href="{{ route('member.login') }}" style="color:black">Login</a><span class="mx-2"></span> | <span class="mx-2"></span>
-                        <a href="{{ route('register') }}"  style="color:black">Register</a>
-                    </ul>
-            </div>
-        {{-- </div> --}}
+        <!--Login FLoating-->
+        <div class="login-floating">
+                <ul class="row  mr-2 p-0 ">
+                    <a href="{{ route('member.login') }}" style="color:black">Login</a><span class="mx-2"></span> | <span class="mx-2"></span>
+                    <a href="{{ route('register') }}"  style="color:black">Register</a>
+                </ul>
+        </div>
+        <!--Search FLoating-->
         <div class="card floating-area p-0 ">
             <div class="card-body row p-2">
-            <div class=" col-1 pl-4" style="padding-top: 1px">
+            <div class=" col-1 pl-3" style="padding-top: 1px">
                 <a id="sidebar-nav" href="#" style="color: black">
                     <i class="fas fa-bars fa-lg"></i>
                 </a>
@@ -145,38 +150,42 @@
                     
             </div>
             </div>
-            
         </div>
+        <!-- Maps -->
         <div id="map" class="p-5" style="width:100%;height:100%"></div>
+        
+        <!-- Navigation Floating-->
         <div class="zoom-button p-0">
             <div class="btn-group-vertical">
                 <button class="btn btn-light" id="center" type="text"><i class="fas fa-map-marker"></i></button> <span class="mb-2"></span>
-                <button class="btn btn-light" id="zoomin" type="text">[ + ]</button> <span class="mb-1"></span>
+                <button class="btn btn-light" id="zoomin" type="text">[ + ]</button><span  style="border-bottom: 1px solid #000"></span>
                 <button class="btn btn-light" id="zoomout" type="text">[ - ]</button>
             </div>
         </div>
+        <!-- Control Floating-->
         <div class="map-controller p-0">
-                <div class="btn-group " role="group" aria-label="map-control">
-                    <button id="s_OSM" type="button" name="mapstyles" value="OSM" class="mapstyles btn btn-default active">Street</button>
-                    <button id="s_SAT" type="button" name="mapstyles" value="SAT" class="mapstyles btn btn-default">Satelite</button>
-                    <button id="s_TER" type="button" name="mapstyles" value="TER" class="mapstyles btn btn-default">Terrain</button>
-                    <button id="s_HIB" type="button" name="mapstyles" value="TER" class="mapstyles btn btn-default">Hibrid</button>
-                </div>
+            <div class="btn-group " role="group" aria-label="map-control">
+                <button id="s_OSM" type="button" name="mapstyles" value="OSM" class="mapstyles btn btn-default active">Street</button>
+                <button id="s_SAT" type="button" name="mapstyles" value="SAT" class="mapstyles btn btn-default">Satelite</button>
+                <button id="s_TER" type="button" name="mapstyles" value="TER" class="mapstyles btn btn-default">Terrain</button>
+                <button id="s_HIB" type="button" name="mapstyles" value="TER" class="mapstyles btn btn-default">Hibrid</button>
+            </div>
         </div>
     </main>
-    <!-- page-content" -->
 </div>
-<!-- page-wrapper -->
+
+    <!-- JQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!--Popper-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
         crossorigin="anonymous"></script>
+    <!--Bootstrap-->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
 
+    <script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.66.2/src/L.Control.Locate.min.js"></script>
+    <!--Custome JS-->
     <script src="/js/sidenav.js"></script>
     <script src="/js/maps.js"></script>
-
-    
 </body>
-
 </html>
