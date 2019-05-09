8cdb806429c51376ad970d55387933229f1df241
bayuguna <putubayuguna@gmail.com>
2019-05-09 16:10:00 +0800

    image upload

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -308,6 +308,49 @@
         </div>
     </div>
 
+<!--Add Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            {{-- <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Lokasi</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div> --}}
+            <div class="modal-body">
+                <div class="card mb-2">
+                    <div class="card-body pt-2 pb-2 pl-3">
+                        <div class="row ">
+                            <div class="col-3">
+                                <img src="/user_img/map-marker.png" width="70px"  alt="">
+                            </div>
+                            <div class="col-9">
+                                <p>Pura Goa Gong</p>
+                                <p>Jalan Goa Gong no 8 xxx yyy zzz</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-1 pr-1 pt-1">
+                        <i class="fa fa-link"></i>
+                    </div>
+                    <div class="col pl-1">
+                        <input type="text" class="form-control form-control-sm" id="sharelink" placeholder="Link">
+                    </div>
+                    
+                </div>
+                <div class="float-right">
+                    <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Batal</button>
+                    <button type="button" class="btn btn-primary btn-sm">Bagikan Link</button>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</div>
+
 <!-- Panorama Modal -->
 @foreach($marker as $data)
 <div id="panoramaModal" class="modal">
