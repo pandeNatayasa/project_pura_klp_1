6f2b5750c32dc11d41c91eaf7ec7adcee46ef04b
bayuguna <putubayuguna@gmail.com>
2019-05-18 15:14:44 +0800

    kontribusi

--- resources/views/user/contribution.blade.php
+++ resources/views/user/contribution.blade.php
@@ -20,7 +20,20 @@
                     <div class="row">
                         @foreach ($contribution as $data)
                         <div class="col-6 col-md-3">
-                            <div class="card" >
+                            @if($data->validate_status == '1')
+                            <div class="span-verify" style="position: absolute; z-index: 1; right: 0; margin-right: 3px;">
+                                <span class="badge badge-success" style=" border-radius: 50%;width: 35px; height: 35px; padding:11px"><i class="fas fa-check"></i></span>
+                            </div>
+                            @elseif($data->validate_status == '2')
+                            <div class="span-verify" style="position: absolute; z-index: 1; right: 0; margin-right: 3px;">
+                                <span class="badge badge-danger" style=" border-radius: 50%;width: 35px; height: 35px; padding:11px"><i class="fas fa-times"></i></span>
+                            </div>
+                            @else
+                            <div class="span-verify" style="position: absolute; z-index: 1; right: 0; margin-right: 3px;">
+                                <span class="badge badge-primary" style=" border-radius: 50%;width: 35px; height: 35px; padding:11px"><i class="fas fa-sync-alt"></i></span>
+                            </div>
+                            @endif
+                            <div class="card  mt-2" >
                                 <img class="card-img-top" src="/user_img/uluwatu.jpg" alt="Card image cap">
                                 <div class="card-body">
                                     <h5 class="card-title text-center">{{$data->temple_name}}</h5>
